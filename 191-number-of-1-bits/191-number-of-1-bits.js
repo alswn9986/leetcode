9ed05bb63ec74ec12f4d4a94ffe4e4113df35240
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-05-26 15:17:24 +0900

    Time: 92 ms (42.35%), Space: 43.7 MB (19.00%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -3,11 +3,6 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    let binary = '';
-    while(n > 0) {
-        binary = (n % 2) + '' + binary;
-        n = parseInt(n / 2);
-    }
-    
-    return binary.split('').filter(x => x == '1').length;
+    const input = n.toString(2).split('');
+    return input.filter(x => x == '1').length;
 };
