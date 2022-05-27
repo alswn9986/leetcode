60d88dc787c65e99bd0beb4c772011b039212c32
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-05-27 10:05:20 +0900

    Time: 80 ms (47.59%), Space: 42.1 MB (52.28%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -5,8 +5,8 @@
 var numberOfSteps = function(num) {
     let count = 0;
     while (num > 0) {
-        if (num % 2 == 0)   num = num / 2;
-        else    num = num - 1;
+        if (num % 2 == 0)   num /= 2;
+        else    num--;
         count++;
     }
     return count;
