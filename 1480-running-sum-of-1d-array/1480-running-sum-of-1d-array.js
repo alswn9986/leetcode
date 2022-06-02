cf51e457b8f062ec2ff1d5ea782984246bedbe74
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-06-02 13:28:44 +0900

    Time: 92 ms (34.11%), Space: 42.7 MB (16.51%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -4,7 +4,7 @@
  */
 var runningSum = function(nums) {
     let sum = 0;
-    let arr = new Array();
+    let arr = [];
     for (let i = 0; i < nums.length; i++) {
         sum = sum + nums[i];
         arr.push(sum);
