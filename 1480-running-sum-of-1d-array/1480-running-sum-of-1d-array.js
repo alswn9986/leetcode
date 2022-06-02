322cd651b0e49968fae3f88c026a7c3eeb4c8b1e
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-06-02 13:28:05 +0900

    Time: 122 ms (5.56%), Space: 42.5 MB (36.75%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let sum = 0;
+    let arr = new Array();
+    for (let i = 0; i < nums.length; i++) {
+        sum = sum + nums[i];
+        arr.push(sum);
+    }
+    return arr;
+};
