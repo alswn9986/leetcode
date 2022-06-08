3fd9ae14ef98e61b1b75b342fc630fd528bf0354
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-06-08 15:42:00 +0900

    Time: 110 ms (11.96%), Space: 42.4 MB (9.78%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var removePalindromeSub = function(s) {
+    // 앞 뒤가 똑같은 경우 palindromic subsequence
+    if (s.length == 0)  return 0;
+    return [...s].reverse().join('') == s ? 1 : 2;
+};
