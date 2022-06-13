714863e89fc3d162bb41d22bccf7328a20df6e77
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-06-13 17:25:12 +0900

    Time: 82 ms (60.66%), Space: 44.1 MB (31.28%) - LeetHub

---
+++ 120-triangle/120-triangle.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    let d = triangle[triangle.length - 1]
+    
+    for (let i = triangle.length - 2; i >= 0; i--) {
+        for (let j = 0; j < triangle.length; j++) {
+            d[j] = triangle[i][j] + Math.min(d[j],d[j + 1]);
+        }
+    }
+    
+    return d[0];
+};
