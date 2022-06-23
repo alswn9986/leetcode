932472593b8e96062aaf19530fd708316cf9da37
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-06-23 14:54:50 +0900

    Time: 75 ms (88.70%), Space: 43.9 MB (57.66%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -4,5 +4,6 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    return nums.sort((x, y) => y - x)[k - 1]
+    nums.sort((n1, n2) => n1 < n2 ? 1 : -1);
+    return nums[k - 1]
 };
