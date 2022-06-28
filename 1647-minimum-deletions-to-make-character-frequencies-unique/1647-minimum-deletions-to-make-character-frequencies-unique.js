088bb78a6f45c72395faf3d02c3898e0eb0fc292
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-06-28 10:35:00 +0900

    Time: 97 ms (96.27%), Space: 46.8 MB (83.20%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.js
@@ -3,32 +3,22 @@
  * @return {number}
  */
 var minDeletions = function(s) {
-    let answer = 0;
-    const arr = s.split('');
-    const dict = {};
-    const mem = [];
-    arr.map(char => {
-        dict[char] = dict[char] ? dict[char] + 1 : 1;
-    })
-    const values = Object.values(dict);
-    values.map(v => {
-        if(mem[v]){
-            let nextPos = v;
-            while(nextPos > 0){
-                if(!mem[nextPos]){
-                    mem[nextPos] = true;
-                    answer += v - nextPos;
-                    break;
-                }else{
-                    nextPos--;
-                }
-            }
-            if(nextPos === 0){
-                answer += v;
-            }
-        }else{
-            mem[v] = true;
+    let arr = Array(26).fill(0)
+    let res = 0
+    
+    for (let i = 0; i < s.length; i++) {
+        let index = s[i].charCodeAt(0) - 'a'.charCodeAt(0)
+        arr[index]++
+    }
+    
+    arr.sort((a, b) => b - a)
+    
+    for(let i=1; i<26; i++){
+        while (arr[i] && arr[i] >= arr[i-1]) {
+            arr[i]--
+            res++
         }
-    })
-    return answer;
+    }
+    
+    return res
 };
