8b48a4d3dde92ebb30d01c793127f2a865eb800c
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-07-05 16:59:09 +0900

    Time: 176 ms (63.68%), Space: 57.8 MB (51.06%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    let arr = [...new Set(nums)];
-    arr = arr.sort((a,b) => a - b);
-    let max = 0;
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-        if (i > 0 && arr[i] === arr[i - 1] + 1) {
-            count++;
+    var set = new Set(nums);
+    var longest = 0;
+    for(x of set){
+        if(!set.has(x-1)){
+            var len = 0;
+            while(set.has(x+len)){
+                len++;
+                longest = Math.max(longest, len);
+            }
         }
-        else {
-            count = 1;
-        }
-        max = Math.max(max, count);
     }
-    return max;
+    return longest;
 };
