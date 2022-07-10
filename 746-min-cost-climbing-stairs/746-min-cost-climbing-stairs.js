d77bc8794d2b3123d1db20f1ab8e412af13585c7
alswn9986 <101205543+alswn9986@users.noreply.github.com>
2022-07-10 21:06:45 +0900

    Time: 289 ms (5.10%), Space: 51.5 MB (5.06%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,7 +3,10 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    for (let i = cost.length - 3; ~i; i--)
+    for (let i = cost.length - 3; ~i; i--) {
+        console.log(i);
         cost[i] += Math.min(cost[i+1], cost[i+2])
+    }
+    console.log(cost)
     return Math.min(cost[0], cost[1])
 };
