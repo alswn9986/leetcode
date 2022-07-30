92405f20cd37d6e4a25641f3e0d8ebbc3e612834
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-07-30 23:00:09 +0900

    Time: 209 ms (86.54%), Space: 59.4 MB (92.31%) - LeetHub

--- 916-word-subsets/916-word-subsets.js
+++ 916-word-subsets/916-word-subsets.js
@@ -3,47 +3,40 @@
  * @param {string[]} words2
  * @return {string[]}
  */
-const wordSubsets = (words1, words2) => {
-
-  const ans = []
-
-  const letterHash = {}
-
-  for (let i = 0; i < words2.length; i++) {
-    const item = counter(words2[i])
-    for (let key in item) {
-      letterHash[key] = Math.max(item[key], letterHash[key] || 0)
+var wordSubsets = function(words1, words2) {
+    let order1 = 0;
+    let temp = new Int8Array(26);
+    let maxWordArr = new Int8Array(26);
+    let wordCount = 0;
+    for (let i = 0; i < words2.length; i++) {
+        for (let j = 0; j < words2[i].length; j++) {
+            temp[words2[i].charCodeAt(j) - 97]++;
+        }
+        for (let k = 0; k < 26; k++) {
+            if (temp[k] > 0 && temp[k] > maxWordArr[k]) {
+                maxWordArr[k] = temp[k];
+                wordCount += maxWordArr[k];
+                if (wordCount > 10) return [];
+            }
+        }
+        wordCount = 0;
+        temp.fill();
     }
-  }
-
-
-  for (let i = 0; i < words1.length; i++) {
-    const item = counter(words1[i])
-    let isOk = true
-    for (let key in letterHash) {
-      const x = item[key] || 0
-      const y = letterHash[key]
-      if (x < y) {
-        isOk = false
-        break
-      }
+    while (words1.length > order1) {
+        let resultArr = new Int8Array(26);
+        for (let i = 0; i < words1[order1].length; i++) {
+            resultArr[words1[order1].charCodeAt(i) - 97]++;
+        }
+        for (let i = 0; i < 26; i++) {
+            if (maxWordArr[i] > 0) {
+                resultArr[i] -= maxWordArr[i] ;
+            }
+        }
+        if (resultArr.some(el => el < 0)) {
+            words1.splice(order1, 1);
+        } else{
+            order1++;
+        }
     }
-    if (isOk) {
-      ans.push(words1[i])
-    }
-  }
-
-  return ans
-
-  function counter (str) {
-    const map = {}
-    for (let i = 0; i < str.length; i++) {
-      const item = str[i]
-      if (!map[item]) {
-        map[item] = 0
-      }
-      map[item]++
-    }
-    return map
-  }
-}
+    return words1;
+};
