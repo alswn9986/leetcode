c9bf2652b14ba60d15d5ec9dff21160906c43cef
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-08-03 09:35:42 +0900

    Time: 328 ms (26.67%), Space: 51.1 MB (50.00%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.js
+++ 729-my-calendar-i/729-my-calendar-i.js
@@ -1,6 +1,10 @@
 
 var MyCalendar = function() {
-    this.events = [];
+    this.head = {start:-Infinity, end:-Infinity, prev: null};
+    this.tail = {start:Infinity, end:Infinity, next: null};
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+    // this.deque
 };
 
 /** 
@@ -9,19 +13,27 @@ var MyCalendar = function() {
  * @return {boolean}
  */
 MyCalendar.prototype.book = function(start, end) {
-    let left = 0;
-    let right = this.events.length -1;
-    while (left <= right) {
-        const mid = left + Math.floor((right - left) / 2);
-
-        if( start < this.events[mid][1]  && this.events[mid][0] < end) return false;
-        
-        if (this.events[mid][1] <= start) left = mid + 1;
-        else  right = mid - 1;
+    let pointer = this.tail;
+    
+    while(pointer.start > start) {
+        pointer = pointer.prev;
     }
-    this.events.push([start, end]);
-    this.events.sort((a,b)=> (a[0] - b[0]) || (a[1] - b[1]) );
-    return true;  
+    if(pointer.start === start) return false;
+    if(pointer.end > start) return false;
+    if(pointer.next.start < end) return false;
+    
+    
+    let node = {
+        start,
+        end,
+        next: pointer.next,
+        prev: pointer,
+    }
+    
+    pointer.next.prev = node;
+    pointer.next = node;
+    
+    return true;
 };
 
 /** 
