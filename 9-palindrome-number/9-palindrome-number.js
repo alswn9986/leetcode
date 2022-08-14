be6933e1dc2b51db75f67e3a06dd6f4fef50e45e
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-08-14 15:32:51 +0900

    Time: 316 ms (29.59%), Space: 50.4 MB (85.83%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -2,14 +2,6 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    const MinNum = -2147483648; //-2^31 값
-    const MaxNum = 2147483647;  //2^31-1 값
-    
-    if(x < MinNum || x > MaxNum) {
-        return false;
-    }
-    
-    const reverse =  x.toString().split("").reverse().join("");
-    return x === Number(reverse);
+var isPalindrome = function (x) {
+    return String(x) === String(x).split('').reverse().join('')
 };
