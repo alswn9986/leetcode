69d297c7fb5a1f94c260269c16c647223a5422a3
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-08-18 17:23:02 +0900

    Time: 77 ms (81.47%), Space: 42.6 MB (58.12%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let result = '';
+    if(strs.length <1) return result;
+    const first = strs[0]; // flower
+    for(let i = 0; i<first.length ; i++){
+        const c = first[i]; // f, l, o, w, e, r 
+        
+        for(let j = 1;j<strs.length;j++){
+            if(strs[j][i] !== c) return result; // f"l"ow, fl"o"w
+        }
+        result += c;
+    }
+    return result;
+};
