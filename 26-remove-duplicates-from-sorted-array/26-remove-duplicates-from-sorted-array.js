d4cf47b44e02aeff4e6ac8b74ca636a95a7f19a4
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-09-01 17:59:40 +0900

    Time: 126 ms (46.99%), Space: 44.6 MB (61.30%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -3,13 +3,22 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    if (nums.length == 0) return 0;
-    var i = 0;
-    for (var j = 1; j < nums.length; j++) {
-        if (nums[j] != nums[i]) {
-            i++;
-            nums[i] = nums[j];
+    
+    
+    if(nums.length === 0) return 0 
+    
+    let i = 0
+    
+    
+    for(let j= 1; j < nums.length; j++){
+        if(nums[j] != nums[i]){ 
+            i++
+            nums[i] = nums[j]
         }
     }
-    return i + 1;
-}
+
+
+    
+return i+1
+    
+};
