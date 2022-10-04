f8eb8c5e71195690444e414a0f2285de432716a0
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-10-04 16:29:41 +0900

    Time: 139 ms (45.99%), Space: 44.7 MB (11.70%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,11 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [i, j];
-            }
-        }
+    let storage = {};
+    
+    for (let [index, num] of nums.entries()) {
+        if (storage[num] !== undefined) return [storage[num], index];
+        storage[target - num] = index; 
     }
 };
