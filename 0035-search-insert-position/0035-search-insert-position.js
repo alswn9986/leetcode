555a9498a32e7725ea561543d87e1acb8078f89c
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-10-28 09:46:55 +0900

    Time: 72 ms (83.81%), Space: 43.4 MB (7.95%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,10 +4,7 @@
  * @return {number}
  */
 var searchInsert = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] >= target) {
-      return i;
-    }
-  }
-  return nums.length;
+    nums.push(target)
+    nums.sort((a, b) => a - b)
+    return nums.indexOf(target)
 };
