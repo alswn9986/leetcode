fd3dcc8519614843e509a75cfa70548154f4a2fd
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-11-05 21:44:51 +0900

    Time: 1551 ms (12.53%), Space: 0B (100.00%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,6 @@
+# Please write a DELETE statement and DO NOT write a SELECT statement.
+# Write your MySQL query statement below
+DELETE FROM Person
+WHERE id IN (
+    SELECT id FROM (SELECT p.id FROM Person p INNER JOIN Person p2 ON  p.Email = p2.Email AND  p.Id > p2.Id) t
+)
