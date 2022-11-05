fe7fb8e222f7df3687c37c3d31bd91972a579ff3
Minju Han <101205543+alswn9986@users.noreply.github.com>
2022-11-05 21:48:56 +0900

    Time: 1117 ms (50.04%), Space: 0B (100.00%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -2,5 +2,11 @@
 # Write your MySQL query statement below
 DELETE FROM Person
 WHERE id IN (
-    SELECT id FROM (SELECT p.id FROM Person p INNER JOIN Person p2 ON  p.Email = p2.Email AND  p.Id > p2.Id) t
+    SELECT id
+    FROM (
+        SELECT p.id
+        FROM Person p
+        INNER JOIN Person p2
+        ON p.Email = p2.Email AND  p.Id > p2.Id
+    ) t
 )
